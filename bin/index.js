56954c06fb109088e378aabc3dca5f5ec64f6d0b
Marcus Paeschke <marcus@mphasize.de>
2014-09-24 17:48:18 +0200

    Added basic generator and sails-ember-blueprints

---
+++ bin/index.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies
+ */
+
+var sailsgen = require( 'sails-generate' ),
+  path = require( 'path' );
+
+//
+// This script exists so we can run our generator
+// directly from the command-line for convenience
+// during development.
+//
+
+var scope = {
+  generatorType: 'ember-blueprints',
+  rootPath: process.cwd(),
+  modules: {
+    'ember-blueprints': path.resolve( __dirname, '../lib' )
+  },
+
+  // For the NEW generator we're generating:
+  generatorName: process.argv[ 2 ],
+};
+
+sailsgen( scope, function ( err ) {
+  if ( err ) throw err;
+
+  // It worked.
+  console.log( 'Done.' );
+} );
