ab5974e3d71571e79c47f22bdae9d999bf402c54
Marcus Paeschke <marcus@mphasize.de>
2015-02-19 11:01:13 +0100

    Adding a flavor of „advanced“ blueprints

---
+++ templates/advanced/api/blueprints/populate.js
@@ -0,0 +1,91 @@
+/**
+ * Module dependencies
+ */
+var util = require( 'util' ),
+	actionUtil = require( './_util/actionUtil' ),
+	pluralize = require( 'pluralize' );
+
+/**
+ * Populate (or "expand") an association
+ *
+ * get /model/:parentid/relation
+ * get /model/:parentid/relation/:id
+ *
+ * @param {Integer|String} parentid  - the unique id of the parent instance
+ * @param {Integer|String} id  - the unique id of the particular child instance you'd like to look up within this relation
+ * @param {Object} where       - the find criteria (passed directly to the ORM)
+ * @param {Integer} limit      - the maximum number of records to send back (useful for pagination)
+ * @param {Integer} skip       - the number of records to skip (useful for pagination)
+ * @param {String} sort        - the order of returned records, e.g. `name ASC` or `age DESC`
+ *
+ * @option {String} model  - the identity of the model
+ * @option {String} alias  - the name of the association attribute (aka "alias")
+ */
+
+module.exports = function expand( req, res ) {
+
+	var Model = actionUtil.parseModel( req );
+	var relation = req.options.alias;
+	if ( !relation || !Model ) return res.serverError();
+
+	// Allow customizable blacklist for params.
+	req.options.criteria = req.options.criteria || {};
+	req.options.criteria.blacklist = req.options.criteria.blacklist || [ 'limit', 'skip', 'sort', 'id', 'parentid' ];
+
+	var parentPk = req.param( 'parentid' );
+
+	// Determine whether to populate using a criteria, or the
+	// specified primary key of the child record, or with no
+	// filter at all.
+	var childPk = actionUtil.parsePk( req );
+	var where = childPk ? [ childPk ] : actionUtil.parseCriteria( req );
+	var skip = actionUtil.parseSkip( req );
+	var limit = actionUtil.parseLimit( req );
+	var sort = actionUtil.parseSort( req );
+
+	var populateOptions = {
+		where: where
+	};
+
+	if ( skip ) populateOptions.skip = skip;
+	if ( limit ) populateOptions.limit = limit;
+	if ( sort ) populateOptions.sort = sort;
+
+	Model
+		.findOne( parentPk )
+		.populate( relation, populateOptions )
+		.exec( function found( err, matchingRecord ) {
+			if ( err ) return res.serverError( err );
+			if ( !matchingRecord ) return res.notFound( 'No record found with the specified id.' );
+			if ( !matchingRecord[ relation ] ) return res.notFound( util.format( 'Specified record (%s) is missing relation `%s`', parentPk, relation ) );
+
+			// Subcribe to instance, if relevant
+			// TODO: only subscribe to populated attribute- not the entire model
+			if ( sails.hooks.pubsub && req.isSocket ) {
+				Model.subscribe( req, matchingRecord );
+				actionUtil.subscribeDeep( req, matchingRecord );
+			}
+
+			// find the model identity and the Collection for this relation
+			var association = _.find( req.options.associations, {
+				alias: relation
+			} );
+			var relationIdentity, documentIdentifier;
+
+			if ( association.type === "model" ) {
+				documentIdentifier = relationIdentity = association.model;
+			} else {
+				relationIdentity = association.collection;
+				documentIdentifier = pluralize( association.collection );
+			}
+
+			var RelatedModel = req._sails.models[ relationIdentity ];
+			if ( !RelatedModel ) throw new Error( util.format( 'Invalid route option, "model".\nI don\'t know about any models named: `%s`', relationIdentity ) );
+
+			var related = Ember.linkAssociations( RelatedModel, matchingRecord[ relation ] );
+
+			var json = {};
+			json[ documentIdentifier ] = related;
+			res.ok( json );
+		} );
+};
---
+++ templates/advanced/api/services/Ember.js
@@ -0,0 +1,136 @@
+/**
+ * Ember service
+ *
+ * @module Ember
+ */
+
+var _ = require( 'lodash' ),
+	pluralize = require( 'pluralize' );
+
+var Ember = {
+	linkAssociations: function ( model, records ) {
+		if ( !Array.isArray( records ) ) records = [ records ];
+		var modelPlural = pluralize( model.identity );
+
+		return _.map( records, function ( record ) {
+			var links = {};
+			_.each( model.associations, function ( assoc ) {
+				if ( assoc.type === "collection" ) {
+					links[ assoc.alias ] = sails.config.blueprints.prefix + "/" + modelPlural + "/" + record.id + "/" + assoc.alias;
+				}
+			} );
+			if ( _.size( links ) > 0 ) {
+				record.links = links;
+			}
+			return record;
+		} );
+	},
+
+	/**
+	 * Prepare records and populated associations to be consumed by Ember's DS.RESTAdapter
+	 *
+	 * @param {Collection} model Waterline collection object (returned from parseModel)
+	 * @param {Array|Object} records A record or an array of records returned from a Waterline query
+	 * @param {Associations} associations Definition of the associations, from `req.option.associations`
+	 * @param {Boolean} sideload Sideload embedded records or reduce them to primary keys?
+	 * @return {Object} The returned structure can be consumed by DS.RESTAdapter when passed to res.json()
+	 */
+	buildResponse: function ( model, records, associations, sideload, associatedRecords ) {
+		sideload = sideload || false;
+		var plural = Array.isArray( records ) ? true : false;
+
+		var modelPlural = pluralize( model.identity );
+		var documentIdentifier = plural ? modelPlural : model.identity;
+		var json = {};
+
+		json[ documentIdentifier ] = plural ? [] : {};
+
+		if ( sideload ) {
+			// prepare for sideloading
+			_.each( associations, function ( assoc ) {
+				// only sideload, when the full records are to be included, more info on setup here https://github.com/Incom/incom-api/wiki/Models:-Defining-associations
+				if ( assoc.include === "record" ) {
+					var assocName = assoc.type === "collection" ? pluralize( assoc.collection ) : pluralize( assoc.model );
+					// initialize jsoning object
+					if ( !json.hasOwnProperty( assoc.alias ) ) {
+						json[ assocName ] = [];
+					}
+				}
+			} );
+		}
+
+		var prepareOneRecord = function ( record ) {
+			// get rid of the record's prototype ( otherwise the .toJSON called in res.send would re-insert embedded records)
+			record = _.create( {}, record.toJSON() );
+			var links = {};
+
+			_.each( associations, function ( assoc ) {
+				var assocName = assoc.type === "collection" ? pluralize( assoc.collection ) : pluralize( assoc.model );
+				var assocModel;
+				if ( assoc.type === "collection" ) {
+					if ( sideload && assoc.include === "record" && record[ assoc.alias ] && record[ assoc.alias ].length > 0 ) {
+						assocModel = sails.models[ assoc.collection ];
+						record[ assoc.alias ] = Ember.linkAssociations( assocModel, record[ assoc.alias ] );
+						json[ assocName ] = json[ assocName ].concat( record[ assoc.alias ] );
+					}
+					if ( assoc.include === "index" && associatedRecords[ assoc.alias ] ) record[ assoc.alias ] = _.reduce( associatedRecords[ assoc.alias ], function ( filtered, rec ) {
+						if ( rec[ model.identity ] === record.id ) filtered.push( rec.id );
+						return filtered;
+					}, [] );
+					// @todo if assoc.include startsWith index: ... fill contents from selected column of join table
+					if ( assoc.include === "link" ) {
+						links[ assoc.alias ] = sails.config.blueprints.prefix + "/" + modelPlural + "/" + record.id + "/" + assoc.alias;
+						delete record[ assoc.alias ];
+					}
+					//record[ assoc.alias ] = _.pluck( record[ assoc.alias ], 'id' );
+				}
+				if ( assoc.type === "model" && record[ assoc.alias ] ) {
+					if ( sideload && assoc.include === "record" ) {
+						assocModel = sails.models[ assoc.model ];
+						record[ assoc.alias ] = Ember.linkAssociations( assocModel, record[ assoc.alias ] );
+
+						json[ assocName ] = json[ assocName ].concat( record[ assoc.alias ] );
+					}
+					if ( assoc.include === "link" ) {
+						links[ assoc.alias ] = sails.config.blueprints.prefix + "/" + modelPlural + "/" + record.id + "/" + assoc.alias;
+						delete record[ assoc.alias ];
+					}
+					// if "index" we're already done...
+				}
+			} );
+			if ( _.size( links ) > 0 ) {
+				record.links = links;
+			}
+			return record;
+		};
+
+		// many or just one?
+		if ( plural ) {
+			_.each( records, function ( record ) {
+				json[ documentIdentifier ] = json[ documentIdentifier ].concat( prepareOneRecord( record ) );
+			} );
+		} else {
+			json[ documentIdentifier ] = prepareOneRecord( records );
+		}
+
+		if ( sideload ) {
+			// filter duplicates in sideloaded records
+			_.each( json, function ( array, key ) {
+				if ( key === documentIdentifier ) return;
+				json[ key ] = _.uniq( array, function ( record ) {
+					return record.id;
+				} );
+			} );
+
+			// add *links* for relationships to sideloaded records
+			_.each( json, function ( array, key ) {
+				if ( key === documentIdentifier ) return;
+
+			} );
+		}
+
+		return json;
+	}
+};
+
+module.exports = Ember;
