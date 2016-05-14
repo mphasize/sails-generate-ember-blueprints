ed15a41552f32adce519a49635ade2901c85e3aa
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-05-14 16:36:05 +0200

    Disactivate CreatedAt and UpdatedAt fields

--- tests/dummy-jsonpi/config/models.js
+++ tests/dummy-jsonpi/config/models.js
@@ -27,6 +27,8 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  migrate: 'drop'
+  migrate: 'drop',
 
+  autoCreatedAt: false,
+  autoUpdatedAt: false
 };
